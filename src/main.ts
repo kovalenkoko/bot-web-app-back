1fdc67771002d91b0ce18f288e1d1528fe12f34c
kiryl-kavalenka <kiryl.kavalenka@innowise-group.com>
2023-04-16 19:17:07 +0300

    fix: add host and port to bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(8080);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0', () => {
+    console.log(`Server started on port: ${port}`);
+  });
 }
 bootstrap();
