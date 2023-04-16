91e38627cb91e4aabd703b8d3384b77799a5516f
kiryl-kavalenka <kiryl.kavalenka@innowise-group.com>
2023-04-16 17:06:54 +0300

    fix: add files to .gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelegrafModule } from 'nestjs-telegraf';
-import * as LocalSession from 'telegraf-session-local';
+//import * as LocalSession from 'telegraf-session-local';
 import { BotUpdate } from './bot/bot.update';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
